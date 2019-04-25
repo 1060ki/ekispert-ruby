919290efed176d44dc0e62d3f910a4ecfc5448a3
Tomoki Ichikawa <ichikawa1060@gmail.com>
2019-04-26 01:40:04 +0900

    Camelizable

--- lib/ekispert/course.rb
+++ lib/ekispert/course.rb
@@ -1,10 +1,14 @@
+require_relative 'camelizable'
+
 module Ekispert
   class Course
+    include Camelizable
+
     class << self
 
       # @see http://docs.ekispert.com/v1/api/search/course/extreme.html
       def get(**args)
-        Ekispert.client.get('search/course/extreme', args)
+        Ekispert.client.get('search/course/extreme', camelize_hash(args))
       end
     end
   end
